a2fc119827ea1a1c5759025673a7268ffb1ad553
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 14:27:25 -0700

    Allow login view to POST and flash on submit validation

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ View functions are mapped to one or more route URLs so Flask knows what logic to
 execute when a client requests a given URL
 """
 
-from flask import render_template
+from flask import flash, redirect, render_template
 
 from app import app
 from app.forms import LoginForm
@@ -32,9 +32,14 @@ def index() -> str:
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}",
+        )
+        return redirect("/index")
     return render_template("login.html", title="Sign In", form=form)
 
 
