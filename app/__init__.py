4f8f84ab9541a09179a5da437e89f6734cc3853a
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 13:38:10 -0700

    Read and apply config in app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask  # noqa: D104
 
+from config import Config
+
 app = Flask(
     __name__,
 )  # Passing __name__ to Flask almost always configures Flask in the correct way
+app.config.from_object(Config)
 
 from app import routes  # noqa: E402, F401
 
