a98a53a3fe2e33ea68ebe6a8bcb7316c61a7e2f7
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 16:23:29 -0700

    Add flask shell context

--- microblog.py
+++ microblog.py
@@ -1,1 +1,16 @@
-from app import app  # noqa: D100, F401
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import app, db
+from app.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context() -> dict:
+    return {
+        "sa": sa,
+        "so": so,
+        "db": db,
+        "User": User,
+        "Post": Post,
+    }
