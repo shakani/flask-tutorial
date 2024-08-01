ebe43c485d9e6cc7588c34a9c6248bf058df7e7f
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 11:47:16 -0700

    Changed index return to display username

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,36 @@
+"""Routes handle the different URLs that the application supports.
+
+Handlers for the app routes are written as Python functions called *view functions*
+View functions are mapped to one or more route URLs so Flask knows what logic to
+execute when a client requests a given URL
+"""
+
 from app import app
 
-"""
-Routes handle the different URLs that the application supports
-Handlers for the app routes are written as Python functions called *view functions*
-View functions are mapped to one or more route URLs so Flask knows what logic to execute when a client requests a given URL
-"""
+user = {"username": "Miguel"}
 
 
 # Home page route
 @app.route("/")
 @app.route("/index")
-def index():
-    return "Hello, World!"
+def index() -> str:
+    """Return index page."""
+    return f"""
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello {user["username"]}! </h1>
+    </body>
+</html>
+"""
 
 
 """
 Decorator pattern: use decorators to register functions as callbacks for certain events
-Here, @app.route decorator creates an association between the URL given as an argument and the function
-When web browser requests a URL ('/' or '/index'), Flask will invoke this function and pass its return value back to the browser as a response
+Here, @app.route decorator creates an association between the URL given as an argument
+ and the function
+When web browser requests a URL ('/' or '/index'), Flask will invoke this function and
+ pass its return value back to the browser as a response
 """
