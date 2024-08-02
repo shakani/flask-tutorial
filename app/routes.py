b5989a52dd563425043ca9954bb2de9dccd06a9d
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 17:06:42 -0700

    Added logout view function

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ execute when a client requests a given URL
 
 import sqlalchemy as sa
 from flask import flash, redirect, render_template, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 from app import app, db
 from app.forms import LoginForm
@@ -38,19 +38,23 @@ def index() -> str:
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
-            sa.select(User).where(User.username == form.username.data)
+            sa.select(User).where(User.username == form.username.data),
         )
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('idnex'))
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("/index"))
 
 """
 Decorator pattern: use decorators to register functions as callbacks for certain events
