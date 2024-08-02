9cd33f4ee03d3059fb92e4d42770549ef43a67f6
Sami Hakani <sami.hakani@gmail.com>
2024-08-01 19:09:11 -0700

    Comment on login_view usage

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view = "login"
+login.login_view = "login" # takes same argument as url_for()
 
 from app import models, routes  # noqa: E402, F401
 
