bc924bd19716d71687555eaa8a83706d0c43ec5b
Sami Hakani <sami.hakani@gmail.com>
2024-08-05 18:15:52 -0700

    Add user registration view function

--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -61,6 +61,20 @@ def logout() -> str:
     logout_user()
     return redirect(url_for("index"))
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register() -> str:
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
 
 """
 Decorator pattern: use decorators to register functions as callbacks for certain events
